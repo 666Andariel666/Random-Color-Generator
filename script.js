7f0ea75618ada91cfad538c354a04cc07ebeecdd
hellfire <hellfire@gmail.com>
2021-03-22 20:28:17 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+'use strict';
+// let boxEvents = document.querySelector('.box').addEventListener('pointerup', function () {
+//     box.classList.toggle('active')
+// })
+
+// const arr = [1, 2, 3]
+// const nums = [4, 5, 6, ...arr]
+// console.log(nums);
+
+// const car = {
+//     model: 'Ford',
+//     year: 2021,
+//     color: 'black'
+// }
+// console.log(Object.entries(car));
+// console.log(Object.values(car));
+// console.log(Object.keys(car));
+
+let box = document.querySelector('body')
+let colorName = document.querySelector('.color-name');
+function getRandomColor(){
+    let letters = "0123456789ABCDEF".split('');
+    let color = "#";
+    for (let i = 0; i < 6; i++){
+        color += letters[Math.floor(Math.random() * 16)]; 
+    }
+    return color; 
+    
+}
+window.addEventListener('pointerdown', function () {
+    box.style.background = getRandomColor();
+    colorName.innerHTML = getRandomColor();
+    console.log(getRandomColor());
+})
